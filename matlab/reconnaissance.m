66527cbee2207314d026040d7ef1fafa14a2561b
AlexandreCouderc <43466716+AlexandreCouderc@users.noreply.github.com>
2019-01-07 08:50:24 +0100

    Add files via upload

--- matlab/reconnaissance.m
+++ matlab/reconnaissance.m
@@ -1,6 +1,5 @@
 function reconnaissance()
-    nb_signes = 5;
-    %d=1;
+% nb_signes = 5;
 % centres_d_1_20 = cell(20);
 % for d=1:20
 %     sums = zeros(2*d,1);
@@ -31,8 +30,10 @@ function reconnaissance()
     
 
 %%%%%% APPRENTISSAGE KPPV %%%%%%
-%     n=10;
-%     m=10;
+%     n=5;
+%     m=5;
+%     nb_signes = 5;
+%     signes = {'doigt', 'ok', 'paume', 'poing', 'pouce'};
 %     
 %     zones_app = cell(nb_signes,1);
 %     for s=1:nb_signes
@@ -52,89 +53,89 @@ function reconnaissance()
 %     end
 
 %%%%%% TEST PROFILS EUCLIDIENS %%%%%%
-accuracy_eucli = zeros(20,1);
-for d=1:20
-    signes = {'doigt', 'ok', 'paume', 'poing', 'pouce'};
-    file = load('dist_eucli_app_d_1_20.mat');
-    centres_app = file.centres_d_1_20(d);
-    centres_app = cell(centres_app{1,1});
-    nb_images_test = 10;
-    nb_correct = 0;
-    for s=1:nb_signes
-        for i=1:nb_images_test
-            path = sprintf('image_test/test_%s_%d.tif',signes{s},i);
-            I_test = imread(path);
-
-            centre_test = dist_eucli(I_test, d);
-
-            proba = zeros(nb_signes,1);
-            for j=1:nb_signes       
-                proba(j,1) = proba_distance(centres_app, centre_test, j, nb_signes);
-            end
-            
-            [~, index] = max(proba);
-            
-            if index == s
-                nb_correct = nb_correct + 1;
-            end
-        end
-    end
-    
-    result = (nb_correct / (nb_images_test*5))*100;
-    accuracy_eucli(d,1) = result;
-end
-plot(1:20,accuracy_eucli)
-
-
-%%%%%% TEST KPPV %%%%%%
-%     n=10;
-%     m=10;
-%     kppv=3;
+% accuracy_eucli = zeros(20,1);
+% for d=1:20
 %     signes = {'doigt', 'ok', 'paume', 'poing', 'pouce'};
-%     file = load('kppv_app_m=10_n=10.mat');
-%     zones_app = file.zones_app;
+%     file = load('dist_eucli_app_d_1_20.mat');
+%     centres_app = file.centres_d_1_20(d);
+%     centres_app = cell(centres_app{1,1});
 %     nb_images_test = 10;
 %     nb_correct = 0;
-%     %path = sprintf('Image/Poing/test_12.tif');
-%     nb_total_images_app = 0;
-%     for s=1:nb_signes
-%         nb_total_images_app = nb_total_images_app + size(zones_app{s,1},1);
-%     end
 %     for s=1:nb_signes
 %         for i=1:nb_images_test
-%             dist_total = zeros(nb_total_images_app,2);
 %             path = sprintf('image_test/test_%s_%d.tif',signes{s},i);
 %             I_test = imread(path);
-%             dist = dist_euc2(I_test, zones_app, n, m);
-%             index=1;
-%             for k=1:nb_signes
-%                 dist_total(index:index-1+size(zones_app{k,1},1),1) = dist{k};
-%                 dist_total(index:index-1+size(zones_app{k,1},1),2) = k;
-%                 index = index + size(zones_app{k,1},1);
-%             end
-%             dist_total_sort = sortrows(dist_total,1);
-%             
-%             for j=1:kppv
-%                 best_match(j) = dist_total_sort(j,2);
+% 
+%             centre_test = dist_eucli(I_test, d);
+% 
+%             proba = zeros(nb_signes,1);
+%             for j=1:nb_signes       
+%                 proba(j,1) = proba_distance(centres_app, centre_test, j, nb_signes);
 %             end
 %             
-%             for j=1:nb_signes
-%                 correct = 0;
-%                 for x=1:kppv
-%                     
-%                     if best_match(x) == j
-%                         correct = correct +1;
-%                     end
-%                 end
-%                 proba((i+(s-1)),j) = correct/kppv;
-%             end
-%             [~, index] = max(proba((i+(s-1)),:));
+%             [~, index] = max(proba);
+%             
 %             if index == s
-%                 nb_correct = nb_correct +1;
+%                 nb_correct = nb_correct + 1;
 %             end
 %         end
 %     end
+%     
 %     result = (nb_correct / (nb_images_test*5))*100;
+%     accuracy_eucli(d,1) = result;
+% end
+% plot(1:20,accuracy_eucli)
+
+
+%%%%% TEST KPPV %%%%%%
+    n=10;
+    m=10;
+    kppv=1;
+    nb_signes=5;
+    signes = {'doigt', 'ok', 'paume', 'poing', 'pouce'};
+    file = load('kppv_app_m=10_n=10.mat');
+    zones_app = file.zones_app;
+    nb_images_test = 10;
+    nb_correct = 0;
+    nb_total_images_app = 0;
+    for s=1:nb_signes
+        nb_total_images_app = nb_total_images_app + size(zones_app{s,1},1);
+    end
+    for s=1:nb_signes
+        for i=1:nb_images_test
+            dist_total = zeros(nb_total_images_app,2);
+            path = sprintf('image_test/test_%s_%d.tif',signes{s},i);
+            I_test = imread(path);
+            dist = dist_euc2(I_test, zones_app, n, m);
+            index=1;
+            for k=1:nb_signes
+                dist_total(index:index-1+size(zones_app{k,1},1),1) = dist{k};
+                dist_total(index:index-1+size(zones_app{k,1},1),2) = k;
+                index = index + size(zones_app{k,1},1);
+            end
+            dist_total_sort = sortrows(dist_total,1);
+            
+            for j=1:kppv
+                best_match(j) = dist_total_sort(j,2);
+            end
+            
+            for j=1:nb_signes
+                correct = 0;
+                for x=1:kppv
+                    
+                    if best_match(x) == j
+                        correct = correct +1;
+                    end
+                end
+                proba((i+(s-1)),j) = correct/kppv;
+            end
+            [~, index] = max(proba((i+(s-1)),:));
+            if index == s
+                nb_correct = nb_correct +1;
+            end
+        end
+    end
+    result = (nb_correct / (nb_images_test*5))*100;
 
 
 
@@ -197,14 +198,14 @@ function [densite] = zoning(I, n, m)
     
     densite_chiffre = zeros(1,n*m);
     
-    digit = I;
-    imshow(digit);
-    for i=1:m
-       line([1 largeur], [nb_px_h*(i) nb_px_h*(i)])
-    end
-    for i=1:n
-       line([1+nb_px_v*(i) 1+nb_px_v*(i)],[1 hauteur])
-    end
+%     digit = I;
+%     imshow(digit);
+%     for i=1:m
+%        line([1 largeur], [nb_px_h*(i) nb_px_h*(i)])
+%     end
+%     for i=1:n
+%        line([1+nb_px_v*(i) 1+nb_px_v*(i)],[1 hauteur])
+%     end
     
     for i=1:m
         for j=1:n
